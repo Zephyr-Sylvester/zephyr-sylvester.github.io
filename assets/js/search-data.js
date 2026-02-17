641d703148d9fb6b441de3ab1a897bf55589e0b1
Zephyr-Sylvester <43252611+Zephyr-Sylvester@users.noreply.github.com>
2026-02-17 03:52:33 +0000

    Deploying to gh-pages from @ Zephyr-Sylvester/zephyr-sylvester.github.io@2a90d3094c8ba701dd24a1f9310f79a6a09da571 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,26 +48,37 @@ ninja.data = [{
             window.location.href = "/blog/2026/what-goes-here/";
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+      },{id: "post-amundsen-sea-dynamics",
         
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: "Amundsen Sea Dynamics",
         
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
+        description: "A literature review of Amundsen Sea polynya dynamics, seasonal ice cycles, and recent trends.",
         section: "Posts",
         handler: () => {
           
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
+            window.location.href = "/blog/2026/amundsen-sea/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+      },{id: "post-krill-expert-group-advances-antarctic-krill-management-framework-at-ccamlr-scar",
         
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'Krill Expert Group Advances Antarctic Krill Management Framework at CCAMLR | SCAR <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "",
         section: "Posts",
         handler: () => {
           
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+            window.open("https://scar.org/scar-news/life-sciences/skeg-news/44ccamlr-publication", "_blank");
+          
+        },
+      },{id: "post-tiny-antarctic-krill-benefit-the-planet-in-big-ways-but-face-a-barrage-of-threats",
+        
+          title: 'Tiny Antarctic Krill Benefit the Planet in Big Ways, but Face a Barrage... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "The bountiful creatures sequester carbon and are a vital food source for marine predators, but their future is uncertain",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://www.smithsonianmag.com/science-nature/tiny-antarctic-krill-benefit-the-planet-in-big-ways-but-face-a-barrage-of-threats-180986128/", "_blank");
           
         },
       },{id: "books-the-godfather",
