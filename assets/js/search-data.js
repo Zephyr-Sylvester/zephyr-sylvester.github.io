6e49eb44f5fc473f48b239557ae79936cbe444d9
Zephyr-Sylvester <43252611+Zephyr-Sylvester@users.noreply.github.com>
2026-02-17 05:55:11 +0000

    Deploying to gh-pages from @ Zephyr-Sylvester/zephyr-sylvester.github.io@33139e51a491b0f70328bb0f2a1a41cdf050f007 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,6 +70,17 @@ ninja.data = [{
             window.open("https://scar.org/scar-news/life-sciences/skeg-news/44ccamlr-publication", "_blank");
           
         },
+      },{id: "post-just-a-moment",
+        
+          title: 'Just a moment... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://only.one/campaign/antarctica-ecoindex", "_blank");
+          
+        },
       },{id: "post-tiny-antarctic-krill-benefit-the-planet-in-big-ways-but-face-a-barrage-of-threats",
         
           title: 'Tiny Antarctic Krill Benefit the Planet in Big Ways, but Face a Barrage... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
